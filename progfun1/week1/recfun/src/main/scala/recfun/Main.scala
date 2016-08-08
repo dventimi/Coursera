4c856f9a544e96b68cc83864dae60e4d76e3be49
David A. Ventimiglia <dventimi@gmail.com>
2016-08-07 20:34:34 -0700

    Completed Balance

--- progfun1/week1/recfun/src/main/scala/recfun/Main.scala
+++ progfun1/week1/recfun/src/main/scala/recfun/Main.scala
@@ -21,8 +21,18 @@ object Main {
   /**
     * Exercise 2
     */
-  def balance(chars: List[Char]): Boolean = ???
-  
+  def balance(chars: List[Char]): Boolean = {
+    def iter (acc: Int, chars: List[Char]): Int =
+      if (chars.isEmpty || acc<0) acc else
+        iter(test(acc, chars), chars.tail)
+    def test (acc: Int, chars: List[Char]): Int =
+      if (chars.isEmpty) acc else
+        if (chars.head=='(') acc + 1 else
+          if (chars.head==')') acc -1 else
+          acc
+    if (iter(0, chars)<0) false else true
+    }
+
   /**
     * Exercise 3
     */
